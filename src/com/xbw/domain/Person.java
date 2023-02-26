f5bfb0fbeed427692212d10d57375c51843aa6d2
xbw <xbw@root.com>
2023-02-26 11:58:38 +0800

    1

---
+++ src/com/xbw/domain/Person.java
@@ -0,0 +1,16 @@
+package com.xbw.domain;
+
+import com.sun.jdi.request.StepRequest;
+
+/**
+ * @projectName: workspaceFroGit01
+ * @package: com.xbw.domain
+ * @className: Person
+ * @author: 小霸王
+ * @description: TODO
+ * @date: 2023/2/26 11:49
+ * @version: 1.0
+ */
+public class Person {
+    private String id;
+}
